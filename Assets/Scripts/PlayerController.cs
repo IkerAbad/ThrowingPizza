5ae07497560b52bba6e9971ea2c7651af8439cf5
Iker Fernández Antón <ikerfernandezanton@gmail.com>
2023-04-21 14:19:22 +0200

    21-04-2023_14-20

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed = 35.0f;
+    public float turnSpeed = 300.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+    }
+}
