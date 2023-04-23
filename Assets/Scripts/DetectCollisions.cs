4bc0546a1741cf03d59949f3d9eaac1ad5055710
Iker Fernández Antón <ikerfernandezanton@gmail.com>
2023-04-23 19:38:24 +0200

    1

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -23,10 +23,5 @@ public class DetectCollisions : MonoBehaviour
             Destroy(gameObject);
             Destroy(other.gameObject);
         }
-        else
-        {
-
-        }
-
     }
 }
